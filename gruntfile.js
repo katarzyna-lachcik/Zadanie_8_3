b30bef44c5a9e6ea1e89f78264e34f623f8da074
Katarzyna <kaisek@poczta.fm>
2017-05-29 17:55:13 +0100

    comma and grunt-contrib-watch

--- gruntfile.js
+++ gruntfile.js
@@ -1,35 +1,35 @@
 module.exports = function(grunt) {
 
   // Project configuration.
-	grunt.initConfig({
-		sass: {
-    		options: {
-    			sourceMap: true
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
     		},
     		dist: {
     			files: {
-    					'css/main.css': 'sass/main.sass'
-       			}
+    			 'css/main.css': 'sass/main.sass'
+       		}
     		}
-    	},
-    	jshint: {
-      		all: ['js/*.js']
-    	}
-    	watch: {
-    		scripts: {
-        		files: ['sass/*.sass'],
-        		tasks: ['sass'],
-        		options: {
-            		spawn: false,
-        		},
-    		}		 
+    },
+    jshint: {
+      all: ['js/*.js']
+    },
+    watch: {
+    	scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+          spawn: false,
+        },
+    	}		 
 		}
 
 	});
   // Load the plugins tasks 
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('watch');
+	grunt.loadNpmTasks('grunt-contrib-watch');
   
   // Default task(s).
 	grunt.registerTask('default', ['sass' , 'jshint' , 'watch']);
